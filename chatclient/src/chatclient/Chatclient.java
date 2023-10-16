bfe4664cf58475b84addbfff5b7c7a28accb0abc
gengram <mygengram@gmail.com>
2023-10-16 11:31:14 -0600

    Proyectos creados

--- chatclient/src/chatclient/Chatclient.java
+++ chatclient/src/chatclient/Chatclient.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package chatsocket;
+package chatclient;
 
 /**
  *
  * @author Alumno
  */
-public class Chatsocket {
+public class Chatclient {
 
     /**
      * @param args the command line arguments
